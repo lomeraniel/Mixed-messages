bca029f9967740b44ebfe4c542db0a04e5f65a0e
lomeraniel <39484218+lomeraniel@users.noreply.github.com>
2021-09-24 10:40:16 +0200

    Messages are in an array

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,20 +1,13 @@
+const messages = [
+    "Great things will happen today.",
+    "You should have stayed in bed today!",
+    "It might rain, make sure you won't shrink.",
+    "You will meet someone important today."
+];
+
 function generateRandomMessage() {
     let i = Math.floor(Math.random() * 3);
-    
-    switch (i) {
-        case 0:
-            return "Great things will happen today.";
-            break;
-        case 1:
-            return "You should have stayed in bed today!";
-            break;
-        case 2: 
-            return "It might rain, make sure you won't shrink.";
-            break;
-        case 3: 
-            return "You will meet someone important today."; 
-            break;
-    }
+    return messages[i];   
 }
 
 console.log(generateRandomMessage());

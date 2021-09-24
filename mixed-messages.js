89f0353189b9ab270b19443b4daaf7d4abd8cec8
lomeraniel <39484218+lomeraniel@users.noreply.github.com>
2021-09-24 12:41:12 +0200

    Combines parts of different sentences (array of arrays)

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,13 +1,22 @@
 const messages = [
-    "Great things will happen today.",
-    "You should have stayed in bed today!",
-    "It might rain, make sure you won't shrink.",
-    "You will meet someone important today."
+    ["Great things", " will happen", " today."],
+    ["You", " should have stayed", " in bed today!"],
+    ["It", " might rain", ", make sure you won't shrink."],
+    ["You", " will meet someone important", " today."]
 ];
 
 function generateRandomMessage() {
-    let i = Math.floor(Math.random() * 3);
-    return messages[i];   
+    let endSentence = [];
+    // message number from the list
+    let i;
+    // Buils the message taking the 3 pieces from the array.
+    for (let j = 0; j < messages[1].length ; j++) {
+        i = Math.floor(Math.random() * 3);
+        //console.log(j+ ", " +i);
+        //console.log(messages[i][j]);
+        endSentence.push(messages[i][j]);
+    }
+    return endSentence.join("");   
 }
 
 console.log(generateRandomMessage());
